207aaaaa1feba6f3aa9935fbb2dbb1dd46247d1e
Kamil-D <kamilosdzialek@op.pl>
2017-12-10 15:09:41 +0100

    LoginController created (empty)

---
+++ src/main/java/com/debtor/dzialek/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.debtor.dzialek.controller;
+
+import de.felixroske.jfxsupport.FXMLController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@FXMLController
+public class LoginController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
