1b8e03e4939381e015a557644fd31b634b4050d9
Kamil-D <kamilosdzialek@op.pl>
2017-12-10 15:09:23 +0100

    Updated debtor main view and added login view (not finished)

---
+++ src/main/java/com/debtor/dzialek/view/LoginView.java
@@ -0,0 +1,9 @@
+package com.debtor.dzialek.view;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+
+@FXMLView("/javafxviews/login.fxml")
+public class LoginView extends AbstractFxmlView {
+
+}
